18ede0d4355ada1773139b2ea256f02f704dbd00
Ben Craig <ben.craig@sita.aero>
2018-09-06 15:57:10 +0100

    Add isWeighted to constructor

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -49,11 +49,11 @@ namespace GradeBook.UserInterfaces
 
             if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, isWeighted);
             }
             else if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, isWeighted);
             }
             else
             {
