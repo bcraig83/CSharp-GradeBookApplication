c4686bae46ce422b717ab53e35857a620e6c0069
Ben Craig <ben.craig@sita.aero>
2018-09-06 15:54:16 +0100

    Implement steps 7.1 and 7.2

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,7 +34,7 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
@@ -42,14 +42,16 @@ namespace GradeBook.UserInterfaces
 
             var name = parts[1];
 
-            //var gradeBook = new BaseGradeBook(name);
             BaseGradeBook gradeBook;
             var type = parts[2];
+
+            var isWeighted = bool.Parse(parts[3]);
 
             if (type == "standard")
             {
                 gradeBook = new StandardGradeBook(name);
-            } else if (type == "ranked")
+            }
+            else if (type == "ranked")
             {
                 gradeBook = new RankedGradeBook(name);
             }
@@ -85,7 +87,8 @@ namespace GradeBook.UserInterfaces
         {
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
+            Console.WriteLine(
+                "Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
